e0f8ee70447bc1744406b248210fd78c0c5b3ede
prat96 <pratheekb96@gmail.com>
2019-05-21 15:33:33 +0300

    Add cats_vs_dogs.py

---
+++ transfer_learning/cats_vs_dogs.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import matplotlib.pylab as plt
+
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+tf.enable_eager_execution()
+
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+from tensorflow.keras import layers
+
+CLASSIFIER_URL ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+IMAGE_RES = 224
+
+model = tf.keras.Sequential([
+    hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+])
+
+import numpy as np
+import PIL.Image as Image
+
+grace_hopper = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
+grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
+print(grace_hopper)
+
+grace_hopper = np.array(grace_hopper)/255.0
+print(grace_hopper.shape)
+
+result = model.predict(grace_hopper[np.newaxis, ...])
+print(result.shape)
+
+predicted_class = np.argmax(result[0], axis=-1)
+print(predicted_class)
+
+labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+imagenet_labels = np.array(open(labels_path).read().splitlines())
+
+plt.imshow(grace_hopper)
+plt.axis('off')
+predicted_class_name = imagenet_labels[predicted_class]
+_ = plt.title("Prediction: " + predicted_class_name.title())
