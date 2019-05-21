9cc8a920d558ba941ba823eaa402e1584b715bc7
prat96 <pratheekb96@gmail.com>
2019-05-21 16:00:08 +0300

    Transfer learning on the cats_vs_dogs dataset

--- transfer_learning/cats_vs_dogs.py
+++ transfer_learning/cats_vs_dogs.py
@@ -6,12 +6,12 @@ pip install "tensorflow_hub==0.4.0"
 pip install -U tensorflow_datasets
 """
 
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import matplotlib.pylab as plt
 
 import tensorflow as tf
+
 tf.logging.set_verbosity(tf.logging.ERROR)
 tf.enable_eager_execution()
 
@@ -20,7 +20,7 @@ import tensorflow_datasets as tfds
 
 from tensorflow.keras import layers
 
-CLASSIFIER_URL ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+CLASSIFIER_URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
 IMAGE_RES = 224
 
 model = tf.keras.Sequential([
@@ -30,11 +30,12 @@ model = tf.keras.Sequential([
 import numpy as np
 import PIL.Image as Image
 
-grace_hopper = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
+grace_hopper = tf.keras.utils.get_file('image.jpg',
+                                       'https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
 grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
 print(grace_hopper)
 
-grace_hopper = np.array(grace_hopper)/255.0
+grace_hopper = np.array(grace_hopper) / 255.0
 print(grace_hopper.shape)
 
 result = model.predict(grace_hopper[np.newaxis, ...])
@@ -43,10 +44,79 @@ print(result.shape)
 predicted_class = np.argmax(result[0], axis=-1)
 print(predicted_class)
 
-labels_path = tf.keras.utils.get_file('ImageNetLabels.txt','https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
+                                      'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
 imagenet_labels = np.array(open(labels_path).read().splitlines())
 
 plt.imshow(grace_hopper)
 plt.axis('off')
 predicted_class_name = imagenet_labels[predicted_class]
 _ = plt.title("Prediction: " + predicted_class_name.title())
+
+"""the transfer learning part"""
+
+splits = tfds.Split.ALL.subsplit(weighted=(80, 20))
+
+splits, info = tfds.load('cats_vs_dogs', with_info=True, as_supervised=True, split=splits)
+
+(train_examples, validation_examples) = splits
+
+num_examples = info.splits['train'].num_examples
+num_classes = info.features['label'].num_classes
+
+for i, example_image in enumerate(train_examples.take(3)):
+    print("Image {} shape: {}".format(i + 1, example_image[0].shape))
+
+
+def format_image(image, label):
+    image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES)) / 255.0
+    return image, label
+
+
+BATCH_SIZE = 32
+
+train_batches = train_examples.shuffle(num_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+image_batch, label_batch = next(iter(train_batches.take(1)))
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+result_batch = model.predict(image_batch)
+
+predicted_class_names = imagenet_labels[np.argmax(result_batch, axis=-1)]
+predicted_class_names
+
+plt.figure(figsize=(10, 9))
+for n in range(30):
+    plt.subplot(6, 5, n + 1)
+    plt.imshow(image_batch[n])
+    plt.title(predicted_class_names[n])
+    plt.axis('off')
+_ = plt.suptitle("ImageNet predictions")
+
+URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/2"
+feature_extractor = hub.KerasLayer(URL,
+                                   input_shape=(IMAGE_RES, IMAGE_RES, 3))
+feature_batch = feature_extractor(image_batch)
+print(feature_batch.shape)
+feature_extractor.trainable = False
+
+model = tf.keras.Sequential([
+    feature_extractor,
+    layers.Dense(2, activation='softmax')
+])
+
+model.summary()
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
+
+EPOCHS = 6
+history = model.fit(train_batches,
+                    epochs=EPOCHS,
+                    validation_data=validation_batches)
+
+
